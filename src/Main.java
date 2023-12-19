587819c63112c856189497190992588ba267f182
zaira <231128@astanait.edu.kz>
2023-12-19 10:20:10 +0600

    First init

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Student student =  new Student();
 
+        student.setName("Maksat");
+        student.setSurname("Alimzhanov");
+        student.setAge(22);
+        student.setGpa(3.5);
+
+        System.out.println(student.toString());
+        Scanner scanner = new Scanner(System.in);
+        Student student1 = new Student(scanner.next(), scanner.next(), scanner.nextInt(), scanner.nextDouble());
+        System.out.println(student1.toString());
     }
+
+
+
 }
